f1d7286e3f6733a1e610f27353cf1efbfc5f12bd
Deepak9313 <108660165+Deepak9313@users.noreply.github.com>
2023-09-06 12:15:15 +0000

    array file inserted

--- array.php
+++ array.php
@@ -3,5 +3,14 @@
 $fruits = array("banana","apple","pear");
 echo ($fruits[0])
 #count is used to count the items in an array
+#array_chink = splits an array into chunks of arrays
+#array_combine = it arrange the one array to key and second to vlaue
+#array_count_values = count all values of an array
+#array_diff = returns the differences
+#array_keys = returns keys of an array
+#array_map = as like a js in map
+#array_merge = merge one or more array into one array
+#array_multisort = sort multiple or multi-dimensional arrays
+#array_pad = insets a specified number of items,with a specified value to an array
 
 ?>
